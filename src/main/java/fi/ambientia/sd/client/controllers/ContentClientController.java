686d1c25e26c4778cb4fc5cad0790d73c69b59d2
Lauri Hiltunen <hiltusenlauri@gmail.com>
2019-09-09 19:57:25 +0300

    Use Swagger to get more friendly UI to API

--- src/main/java/fi/ambientia/sd/client/controllers/ContentClientController.java
+++ src/main/java/fi/ambientia/sd/client/controllers/ContentClientController.java
@@ -2,11 +2,14 @@ package fi.ambientia.sd.client.controllers;
 
 import fi.ambientia.sd.client.clients.FeignContentClient;
 import fi.ambientia.sd.client.clients.RibbonContentClient;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Api
 @RestController
 @RequestMapping
 public class ContentClientController {
